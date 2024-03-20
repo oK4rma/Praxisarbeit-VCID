d24d9f7d850c3630e795b0591ea0391b37e31702
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-20 09:35:56 +0100

    Update models.py

--- flask/app/models.py
+++ flask/app/models.py
@@ -4,12 +4,11 @@ from datetime import datetime, timedelta
 from flask import url_for
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from app import db, login
-
 
 class CollectionMixin(object):
     @staticmethod
+    # Konvertiert eine SQL-Alchemy-Query in ein Wörterbuch mit einer Liste von Elementen.
     def to_collection_dict(query):
         resources = query.all()
         data = {
@@ -17,7 +16,7 @@ class CollectionMixin(object):
         }
         return data
 
-
+# Benutzermodell, erbt von UserMixin für Authentifizierung und von db.Model für SQLAlchemy.
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -27,12 +26,15 @@ class User(UserMixin, db.Model):
     token = db.Column(db.String(32), index=True, unique=True)
     token_expiration = db.Column(db.DateTime)
 
+    # Erzeugt einen Passwort-Hash aus einem Passwort.
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+     # Überprüft das Passwort gegen den Passwort-Hash.
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    # Erstellt oder erneuert einen Authentifizierungstoken.
     def get_token(self, expires_in=3600):
         now = datetime.utcnow()
         if self.token and self.token_expiration > now + timedelta(seconds=60):
@@ -41,20 +43,24 @@ class User(UserMixin, db.Model):
         self.token_expiration = now + timedelta(seconds=expires_in)
         db.session.add(self)
         return self.token
-
+        
+    # Widerruft den aktuellen Authentifizierungstoken.
     def revoke_token(self):
         self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
 
     @staticmethod
+    # Überprüft die Gültigkeit eines Authentifizierungstokens.
     def check_token(token):
         user = User.query.filter_by(token=token).first()
         if user is None or user.token_expiration < datetime.utcnow():
             return None
         return user
-
+        
+    # Überprüft, ob der Benutzer eine Reservierung an einem bestimmten Datum hat.
     def reservation(self, date):
         return self.reservations.filter_by(date=date).first()
 
+    # Konvertiert Benutzerdaten in ein Wörterbuchformat.
     def to_dict(self):
         data = {
             "id": self.id,
@@ -62,15 +68,17 @@ class User(UserMixin, db.Model):
         }
         return data
 
+    # Repräsentiert das Benutzerobjekt als String.
     def __repr__(self):
         return f"<User {self.username}>"
 
 
 @login.user_loader
+# Lädt einen Benutzer anhand seiner ID für die Flask-Login-Integration.
 def load_user(id):
     return User.query.get(int(id))
 
-
+# Modell für ein Mietfahrzeug.
 class rentalvehicle(db.Model, CollectionMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float)
@@ -79,13 +87,16 @@ class rentalvehicle(db.Model, CollectionMixin):
         "Reservation", backref="rental_vehicle", lazy="dynamic"
     )
 
+    # Überprüft, ob das Fahrzeug an einem bestimmten Datum reserviert ist.
     def is_reserved(self, date):
         return self.reservations.filter_by(date=date).first()
 
+    # Fügt eine neue Reservierung hinzu, wenn das Fahrzeug an dem Datum nicht reserviert ist.
     def reserve(self, date, user):
         if not self.is_reserved(date):
             self.reservations.append(Reservation(date=date, user=user))
 
+    # Entfernt eine Reservierung an einem bestimmten Datum für einen Benutzer.
     def free(self, date, user):
         userReservation = self.reservations.filter_by(date=date, user=user).first()
         if userReservation:
@@ -93,7 +104,8 @@ class rentalvehicle(db.Model, CollectionMixin):
             return userReservation
         else:
             return None
-
+            
+    # Konvertiert Fahrzeugdaten in ein Wörterbuchformat mit Links zu relevanten API-Routen.
     def to_dict(self):
         data = {
             "id": self.id,
@@ -133,5 +145,6 @@ class Reservation(db.Model, CollectionMixin):
         }
         return data
 
+    # Repräsentiert das Reservierungsobjekt als String für leichtere Identifizierung in Debugging- und Logging-Prozessen.
     def __repr__(self):
         return f"<Reservation {self.id}>"
