c41f986cba4278afbf6bcca0f77d838cb90a34ce
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-20 09:53:11 +0100

    Add files via upload

---
+++ flask/app/models.py
@@ -0,0 +1,150 @@
+import base64
+import os
+from datetime import datetime, timedelta
+from flask import url_for
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import db, login
+
+class CollectionMixin(object):
+    @staticmethod
+    # Konvertiert eine SQL-Alchemy-Query in ein Wörterbuch mit einer Liste von Elementen.
+    def to_collection_dict(query):
+        resources = query.all()
+        data = {
+            "items": [item.to_dict() for item in resources],
+        }
+        return data
+
+# Benutzermodell, erbt von UserMixin für Authentifizierung und von db.Model für SQLAlchemy.
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    reservations = db.relationship("Reservation", backref="user", lazy="dynamic")
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    # Erzeugt einen Passwort-Hash aus einem Passwort.
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+     # Überprüft das Passwort gegen den Passwort-Hash.
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # Erstellt oder erneuert einen Authentifizierungstoken.
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8")
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+        
+    # Widerruft den aktuellen Authentifizierungstoken.
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    # Überprüft die Gültigkeit eines Authentifizierungstokens.
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+        
+    # Überprüft, ob der Benutzer eine Reservierung an einem bestimmten Datum hat.
+    def reservation(self, date):
+        return self.reservations.filter_by(date=date).first()
+
+    # Konvertiert Benutzerdaten in ein Wörterbuchformat.
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "username": self.username,
+        }
+        return data
+
+    # Repräsentiert das Benutzerobjekt als String.
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+@login.user_loader
+# Lädt einen Benutzer anhand seiner ID für die Flask-Login-Integration.
+def load_user(id):
+    return User.query.get(int(id))
+
+# Modell für ein Mietfahrzeug.
+class rentalvehicle(db.Model, CollectionMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    info = db.Column(db.String(256))
+    reservations = db.relationship(
+        "Reservation", backref="rental_vehicle", lazy="dynamic"
+    )
+
+    # Überprüft, ob das Fahrzeug an einem bestimmten Datum reserviert ist.
+    def is_reserved(self, date):
+        return self.reservations.filter_by(date=date).first()
+
+    # Fügt eine neue Reservierung hinzu, wenn das Fahrzeug an dem Datum nicht reserviert ist.
+    def reserve(self, date, user):
+        if not self.is_reserved(date):
+            self.reservations.append(Reservation(date=date, user=user))
+
+    # Entfernt eine Reservierung an einem bestimmten Datum für einen Benutzer.
+    def free(self, date, user):
+        userReservation = self.reservations.filter_by(date=date, user=user).first()
+        if userReservation:
+            self.reservations.remove(userReservation)
+            return userReservation
+        else:
+            return None
+            
+    # Konvertiert Fahrzeugdaten in ein Wörterbuchformat mit Links zu relevanten API-Routen.
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "price": self.price,
+            "info": self.info,
+            "_links": {
+                "self": url_for("api.get_vehicle", id=self.id),
+                "reservations": url_for("api.get_vehicle_reservations", id=self.id),
+                "reserved": url_for(
+                    "api.get_vehicle_reserved", id=self.id, date="2023-01-01"
+                ),
+            },
+        }
+        return data
+
+    def __repr__(self):
+        return f"<rentalvehicle {self.id}>"
+
+
+class Reservation(db.Model, CollectionMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    rental_vehicle_id = db.Column(db.Integer, db.ForeignKey("rentalvehicle.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "date": self.date,
+            "rental_vehicle_id": self.rental_vehicle_id,
+            "user_id": self.user_id,
+            "_links": {
+                "self": url_for("api.get_reservation", id=self.id),
+                "user": url_for("api.get_reservation_user", id=self.id),
+                "vehicle": url_for("api.get_reservation_vehicle", id=self.id),
+            },
+        }
+        return data
+
+    # Repräsentiert das Reservierungsobjekt als String für leichtere Identifizierung in Debugging- und Logging-Prozessen.
+    def __repr__(self):
+        return f"<Reservation {self.id}>"
---
+++ flask/app/rental/routes.py
@@ -0,0 +1,204 @@
+import datetime
+from flask import flash, redirect, render_template, url_for, abort
+from flask_login import current_user, login_required
+from sqlalchemy import select
+from app import db
+from app.models import rentalvehicle, Reservation, User
+from app.rental import bp
+from app.rental.calendar import rentalCalendar
+from app.rental.forms import BaseForm
+
+# Erstellt eine Instanz von rentalCalendar für die Verwendung in den Routen.
+Calendar = rentalCalendar()
+
+
+@bp.route("/")
+@login_required
+# Zeigt die Hauptseite der Anwendung an, inklusive eines Kalenders und verfügbarer Fahrzeuge.
+def index():
+    # Erstellt ein Basisformular für mögliche Aktionen auf der Seite.
+    form = BaseForm()
+    
+    # Ermittelt das aktuelle Datum und teilt es in Jahr, Monat und den heutigen Tag auf.
+    year = datetime.date.today().year
+    month = datetime.date.today().month
+    today = datetime.date.today()
+
+    # Ruft alle verfügbaren Fahrzeuge aus der Datenbank ab.
+    vehicles = db.session.scalars(select(rentalvehicle))
+
+    # Gibt die Hauptseite mit den entsprechenden Daten für das Rendering zurück.
+    return render_template(
+        "pages/index.html",
+        title="Home",
+        year=year,
+        month=month,
+        activeDate=today,
+        today=today,
+        # Ruft Kalendertage für den aktuellen Monat ab.
+        weeks=Calendar.get_days(month, year),
+        vehicles=vehicles,
+        form=form,
+    )
+
+
+@bp.route("/rental/<date>")
+@login_required
+# Zeigt die Hauptseite für ein spezifisches Datum an, ermöglicht die Anzeige von Fahrzeugreservierungen.
+def date(date):
+    # Erstellt erneut ein Basisformular.
+    form = BaseForm()
+    try:
+        # Versucht, das übergebene Datum zu interpretieren.
+        activeDate = datetime.date.fromisoformat(date)
+    except ValueError:
+        # Gibt einen 404 Fehler zurück, falls das Datum ungültig ist.
+        abort(404)
+
+    # Ermittelt das heutige Datum.
+    today = datetime.date.today()
+    # Verhindert die Anzeige für Daten in der Vergangenheit.
+    if activeDate < today:
+        abort(404)
+
+    # Ermittelt Jahr und Monat des aktuellen Datums.
+    year = datetime.date.today().year
+    month = datetime.date.today().month
+    # Ruft alle Fahrzeuge ab.
+    vehicles = db.session.scalars(select(rentalvehicle))
+
+    # Gibt die Seite mit den Daten für das angefragte Datum zurück.
+    return render_template(
+        "pages/index.html",
+        title="Home",
+        year=year,
+        month=month,
+        today=today,
+        activeDate=activeDate,
+        # Kalenderdaten für den Monat.
+        weeks=Calendar.get_days(month, year),
+        vehicles=vehicles,
+        form=form,
+    )
+
+
+@bp.route("/reserve/<vehicle>/<day>", methods=["POST"])
+@login_required
+# Ermöglicht es, ein Fahrzeug für einen bestimmten Tag zu reservieren.
+def reserve(vehicle, day):
+    # Verwendet das Basisformular für die Reservierungsaktion.
+    form = BaseForm()
+    # Interpretiert das angegebene Datum.
+    today = datetime.date.today()
+    # Überprüft, ob das Formular korrekt abgesendet wurde.
+    dayDate = datetime.date.fromisoformat(day)
+    # Verhindert Reservierungen in der Vergangenheit.
+    if form.validate_on_submit():
+        if dayDate < today:
+            flash("Cannot make reservations in the past")
+            return redirect(url_for("rental.index"))
+
+        # Sucht das spezifizierte Fahrzeug.
+        vehicle = rentalvehicle.query.filter_by(id=vehicle).first()
+
+        # Fehlerbehandlung, falls kein Fahrzeug gefunden wurde.
+        if vehicle is None:
+            flash(f"vehicles {vehicle} not found.")
+            return redirect(url_for("rental.index"))
+
+        # Sucht den aktuellen Benutzer.
+        user = User.query.filter_by(id=current_user.id).first()  # type: ignore
+        if user is None:
+            raise ValueError("User not found")
+
+        # Überprüft, ob bereits eine Reservierung für diesen Tag besteht.
+        if user.reservation(dayDate):
+            flash(
+                "Cannot reserve vehicles. You can only have 1 vehicles per day", "error"
+            )
+            return redirect(url_for("rental.index"))
+
+        # Führt die Reservierung durch.
+        vehicle.reserve(dayDate, current_user)
+        flash(f"vehicles {vehicle} reserved!")
+        db.session.commit()
+
+        return redirect(url_for("rental.index"))
+    else:
+        return redirect(url_for("rental.index"))
+
+
+@bp.route("/free/<vehicle>/<day>", methods=["POST"])
+@login_required
+# Ermöglicht es, eine Reservierung für ein Fahrzeug an einem bestimmten Tag aufzuheben.
+def free(vehicle, day):
+    # Verwendet erneut das Basisformular für diese Aktion.
+    form = BaseForm()
+    # Interpretiert das angegebene Datum.
+    today = datetime.date.today()
+     # Überprüft, ob das Formular korrekt abgesendet wurde.
+    dayDate = datetime.date.fromisoformat(day)
+    # Verhindert Änderungen an Reservierungen in der Vergangenheit.
+    if form.validate_on_submit():
+        if dayDate < today:
+            flash("Cannot make changes to reservations in the past")
+            return redirect(url_for("rental.index"))
+
+        # Sucht das spezifizierte Fahrzeug.
+        vehicle = rentalvehicle.query.filter_by(id=vehicle).first()
+        # Fehlerbehandlung, falls kein Fahrzeug gefunden wurde.
+        if vehicle is None:
+            flash(f"vehicles {vehicle} not found.")
+            return redirect(url_for("rental.index"))
+
+        # Versucht, die Reservierung aufzuheben.
+        userReservation = vehicle.free(dayDate, current_user)
+        # Falls erfolgreich, löscht die Reservierung aus der Datenbank.
+        if userReservation is not None:
+            db.session.delete(userReservation)
+            db.session.commit()
+            flash(f"vehicles {vehicle} is now free!")
+        # Fehlermeldung, falls keine Reservierung für den Benutzer existiert.
+        else:
+            flash(f"vehicles {vehicle} is not reserved by you.", "error")
+        return redirect(url_for("rental.index"))
+    else:
+        return redirect(url_for("rental.index"))
+
+
+@bp.route("/account")
+@login_required
+# Zeigt die Kontoinformationsseite mit Übersicht der Fahrzeugreservierungen an.
+def account():
+    # Ruft alle Fahrzeuge ab.
+    vehicles = db.session.scalars(select(rentalvehicle)).all()
+    # Zählt die Fahrzeuge.
+    vehicleCount = len(list(vehicles))
+    # Ermittelt das heutige Datum.
+    today = datetime.date.today()
+    # Zählt belegte Fahrzeuge.
+    occupied = len(list(filter(lambda x: x.is_reserved(today), vehicles)))
+    # Berechnet die Belegungsrate.
+    occupation = round((occupied / vehicleCount) * 100, 2)
+
+    # Ruft alle Reservierungen bis zum heutigen Tag ab.
+    reservations = db.session.scalars(
+        select(Reservation)
+        .filter(Reservation.date <= today)
+        .join(rentalvehicle)
+        .join(User)
+    ).all()
+
+    # Berechnet den Gesamterlös.
+    revenue = sum(map(lambda x: x.rental_vehicle.price, reservations))
+
+    # Gibt die Kontoseite mit den gesammelten Daten zurück.
+    return render_template(
+        "pages/account.html",
+        title="account",
+        vehicles=vehicleCount,
+        occupied=occupied,
+        occupation=occupation,
+        reservations=reservations,
+        revenue=revenue,
+    )
