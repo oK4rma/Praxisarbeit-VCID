6f8fb7278fee26c493e7be30d6d994e0636ee178
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-20 08:44:05 +0100

    Update forms.py

--- flask/app/rental/forms.py
+++ flask/app/rental/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 
-
+# Erstellt eine Basisformularklasse, die von FlaskForm erbt.
 class BaseForm(FlaskForm):
+    # Definiert den Submit-Button mit einem neutralen Button-Stil.
     submit = SubmitField("Submit", render_kw={"class": "btn btn-neutral"})
