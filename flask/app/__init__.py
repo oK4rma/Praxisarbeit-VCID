ef8bc8e7af95d7ecbd4184aa66c415050ce0dba5
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-20 09:33:00 +0100

    Update __init__.py

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -7,47 +7,46 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from logging.handlers import RotatingFileHandler
 from sqlalchemy import select
-
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+# Bestimmt die Ansicht, die für nicht authentifizierte Benutzer angezeigt wird.
 login.login_view = "auth.login"  # type: ignore
 doc = ApiDoc()
 
-
+# Erstellt und konfiguriert die Flask-Anwendung.
 def create_app(config=Config):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-
-    # We can override this, for tests for example
+    # Lädt die Konfiguration.
     app.config.from_object(config)
 
+    # Initialisiert Erweiterungen mit der Anwendung.
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
 
+    # Importiert und registriert Blueprints für verschiedene Anwendungsteile
     from app import models
-
     from app.errors import bp as errors_bp
-
+    # Registriert das Blueprint für Fehlerbehandlung.
     app.register_blueprint(errors_bp)
 
     from app.rental import bp as rental_bp
-
+    # Registriert das Blueprint für Mietvorgänge.
     app.register_blueprint(rental_bp)
 
     from app.auth import bp as auth_bp
-
+    # Registriert das Blueprint für Authentifizierung.
     app.register_blueprint(auth_bp, url_prefix="/auth")
 
     from app.api import bp as api_bp
-
+    # Registriert das Blueprint für die API.
     app.register_blueprint(api_bp, url_prefix="/api")
 
-    doc.init_app(app)
-
+    # Konfiguriert das Logging.
     if not app.debug and not app.testing:
         if app.config["LOG_TO_STDOUT"]:
             stream_handler = logging.StreamHandler()
@@ -71,17 +70,18 @@ def create_app(config=Config):
         app.logger.setLevel(logging.INFO)
         app.logger.info("App startup")
 
-    # In a testing environment we want full control over the database
+    # Erstellt alle Datenbanktabellen, wenn nicht im Testmodus.
     if not app.testing:
         with app.app_context():
             # Create all database tables
             db.create_all()
 
-            # TODO: This is to handle currently out of scope admin functionality.
+            # Initialisiert Standard-Fahrzeuge, falls noch keine vorhanden sind.
             if not db.session.execute(select(models.rentalvehicle)).first():
                 # Create default rental vehicles
                 for x in range(1, 4):
                     vehicle = models.rentalvehicle(id=x, price=1, info=f"vehicles {x}")
                     db.session.add(vehicle)
                 db.session.commit()
+    # Gibt die konfigurierte Flask-Anwendung zurück.
     return app
