d79f9bb51d6e76b76da2fb0ea254b96ad9cb4304
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-20 08:40:30 +0100

    Update __init__.py

--- flask/app/errors/__init__.py
+++ flask/app/errors/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+# Erstellt ein Blueprint namens 'errors' für die Fehlerbehandlung in der Anwendung.
 bp = Blueprint("errors", __name__)
 # Importiert verschiedene Module (handlers) nach der Blueprint-Erstellung.
 from app.errors import handlers  # noqa: E402, F401
