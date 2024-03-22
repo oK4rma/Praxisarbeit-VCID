797360d54b983a30e9227206c48d281e7eaa6952
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-22 20:46:30 +0100

    Update cli.py

--- flask/app/cli.py
+++ flask/app/cli.py
@@ -2,3 +2,15 @@ from flask_apidoc.commands import GenerateApiDoc
 
 def register(app):
     @app.cli.group()
+     # Definiert eine neue Befehlsgruppe 'docs' im Flask CLI.
+    def docs():
+        """API documentation commands."""
+        # Dient als Platzhalter, da 'docs' als Gruppe für Unterbefehle fungiert.
+        pass
+
+    @docs.command()
+     # Definiert den 'generate'-Befehl innerhalb der 'docs'-Gruppe.
+    def generate():
+        """Compile apidoc"""
+        # Ruft die Funktion zur Generierung der API-Dokumentation auf, speichert die Ergebnisse im angegebenen Verzeichnis.
+        GenerateApiDoc("./app", "app/static/docs").run()
