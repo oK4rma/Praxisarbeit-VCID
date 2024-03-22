8bd4b4566b77c3a424c11d138f2c6a15036d6993
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-22 09:22:32 +0100

    Update routes.py

--- flask/app/rental/routes.py
+++ flask/app/rental/routes.py
@@ -81,6 +81,7 @@ def date(date):
         form=form,
     )
 
+
 @bp.route("/reserve/<vehicle>/<day>", methods=["POST"])
 @login_required
 # Ermöglicht es, ein Fahrzeug für einen bestimmten Tag zu reservieren.
@@ -117,11 +118,6 @@ def reserve(vehicle, day):
             )
             return redirect(url_for("rental.index"))
 
-        # Überprüft, ob der Tag im Kalender als reserviert markiert ist.
-        if Calendar.is_reserved(dayDate):
-            flash("Cannot reserve vehicles. The selected date is already reserved.", "error")
-            return redirect(url_for("rental.index"))
-
         # Führt die Reservierung durch.
         vehicle.reserve(dayDate, current_user)
         flash(f"vehicles {vehicle} reserved!")
@@ -130,6 +126,7 @@ def reserve(vehicle, day):
         return redirect(url_for("rental.index"))
     else:
         return redirect(url_for("rental.index"))
+
 
 @bp.route("/free/<vehicle>/<day>", methods=["POST"])
 @login_required
