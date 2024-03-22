ccdfc02bac6c23eb2d6d2ae3c100a6031d7e4fa4
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-22 20:56:53 +0100

    Update car.py

--- flask/car.py
+++ flask/car.py
@@ -2,5 +2,3 @@ from app import create_app
 
 # Erstellt eine Flask-Anwendung mit Standardkonfiguration.
 app = create_app()
-# Registriert benutzerdefinierte CLI-Befehle bei der Anwendung
-cli.register(app)
