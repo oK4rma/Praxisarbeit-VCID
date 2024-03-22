bfbc1e472ec5fc8bc73f8df4aeac3fe7d7c343cf
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-22 09:12:08 +0100

    Update account.py

--- flask/app/api/account.py
+++ flask/app/api/account.py
@@ -23,12 +23,21 @@ def get_account():
     occupation = round((occupied / vehicleCount), 2)
 
     # Ruft alle Reservierungen ab, die bis heute erfolgt sind, inklusive Verknüpfungen zu Fahrzeugen und Benutzern.
-    reservations = db.session.scalars(
-        select(Reservation)
-        .filter(Reservation.date <= today)
-        .join(rentalvehicle)
-        .join(User)
-    ).all()
+    if current_user.username != "admin":
+        # Ruft alle Reservierungen bis zum heutigen Tag ab.
+        reservations = db.session.scalars(
+            select(Reservation)
+            .filter(Reservation.date <= today)
+            .join(rentalvehicle)
+            .join(User)
+        ).all()
+    else:
+        reservations = db.session.scalars(
+            select(Reservation)
+            .filter(Reservation.date <= today)
+            .join(rentalvehicle)
+            .join(User)
+    ).all()   
 
      # Berechnet den Gesamterlös aus den Reservierungen.
     revenue = sum(map(lambda x: x.rental_vehicle.price, reservations))
