38a1e1ab17bcab1a6909ebe11e5c578a3507e230
oK4rma <150010384+oK4rma@users.noreply.github.com>
2024-03-22 20:14:21 +0100

    Delete flask directory

--- flask/app/rental/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-# Erstellt ein Blueprint namens 'rental' für die Verwaltung von Mietvorgängen in der Anwendung. 
-bp = Blueprint("rental", __name__)
-# Importiert verschiedene Module (routes) nach der Blueprint-Erstellung.
-from app.rental import routes  # noqa: E402, F401
--- flask/app/rental/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-
-# Erstellt eine Basisformularklasse, die von FlaskForm erbt.
-class BaseForm(FlaskForm):
-    # Definiert den Submit-Button mit einem neutralen Button-Stil.
-    submit = SubmitField("Submit", render_kw={"class": "btn btn-neutral"})
